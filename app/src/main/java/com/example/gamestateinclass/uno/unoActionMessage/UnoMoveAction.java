ecd252c979acc2f54623eb4f90ae4274407be071
c1ei <paguirig25@up.edu>
2022-11-11 16:50:02 -0800

    Update UnoMoveAction.java

--- app/src/main/java/com/example/gamestateinclass/uno/unoActionMessage/UnoMoveAction.java
+++ app/src/main/java/com/example/gamestateinclass/uno/unoActionMessage/UnoMoveAction.java
@@ -1,6 +1,16 @@
 package com.example.gamestateinclass.uno.unoActionMessage;
 
 import com.example.gamestateinclass.game.GameFramework.actionMessage.GameAction;
+import com.example.gamestateinclass.game.GameFramework.players.GamePlayer;
 
 public class UnoMoveAction extends GameAction {
+
+    /**
+     * constructor for GameAction
+     *
+     * @param player the player who created the action
+     */
+    public UnoMoveAction(GamePlayer player) {
+        super(player);
+    }
 }
