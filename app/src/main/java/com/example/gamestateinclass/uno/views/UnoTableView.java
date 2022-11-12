49d82dc792ed189f1343724ed4d729e44b3d0abc
millerl23 <112990842+millerl23@users.noreply.github.com>
2022-11-12 14:29:26 -0800

    Updated arrowPos comments

--- app/src/main/java/com/example/gamestateinclass/uno/views/UnoTableView.java
+++ app/src/main/java/com/example/gamestateinclass/uno/views/UnoTableView.java
@@ -126,7 +126,7 @@ public class UnoTableView extends FlashSurfaceView {
 
 		switch (arrowPos){
 			case 0:
-				arrowPath.moveTo(getWidth()/2, (getHeight()/8)*7); // Beginning location center of ~3/4 down the screen
+				arrowPath.moveTo(getWidth()/2, (getHeight()/8)*7); // Beginning location center of just over 3/4 down the view
 				arrowPath.lineTo(getWidth()/2 - 60, (getHeight()/8)*7); // Tip of arrow pointing left
 				arrowPath.lineTo(getWidth()/2 - 20, (getHeight()/8)*7 - 20);
 				arrowPath.lineTo(getWidth()/2 - 20, (getHeight()/8)*7 - 10);
@@ -139,7 +139,7 @@ public class UnoTableView extends FlashSurfaceView {
 				break;
 
 			case 1:
-				arrowPath.moveTo(getWidth()/4, (getHeight()/2)); // Beginning location center of ~3/4 down the screen
+				arrowPath.moveTo(getWidth()/4, (getHeight()/2)); // Beginning location center of ~1/4 in from left of view
 				arrowPath.lineTo(getWidth()/4, (getHeight()/2)-60); // Tip of arrow pointing up
 				arrowPath.lineTo(getWidth()/4+20, (getHeight()/2)-20);
 				arrowPath.lineTo(getWidth()/4+10, (getHeight()/2)-20);
@@ -151,7 +151,7 @@ public class UnoTableView extends FlashSurfaceView {
 				arrowPath.lineTo(getWidth()/4, (getHeight()/2)-60); // Tip of arrow pointing up
 				break;
 			case 2:
-				arrowPath.moveTo(getWidth()/2, (getHeight()/8)*3); // Beginning location center of ~1/4 down the screen
+				arrowPath.moveTo(getWidth()/2, (getHeight()/8)*3); // Beginning location center of just over 1/4 down the view
 				arrowPath.lineTo(getWidth()/2 + 60, (getHeight()/8)*3); // Tip of arrow pointing right
 				arrowPath.lineTo(getWidth()/2 + 20, (getHeight()/8)*3 + 20);
 				arrowPath.lineTo(getWidth()/2 + 20, (getHeight()/8)*3 + 10);
@@ -164,7 +164,7 @@ public class UnoTableView extends FlashSurfaceView {
 
 				break;
 			case 3:
-				arrowPath.moveTo((getWidth()/4)*3, (getHeight()/2)); // Beginning location center of ~3/4 down the screen
+				arrowPath.moveTo((getWidth()/4)*3, (getHeight()/2)); // Beginning location center of ~3/4 in from left of view
 				arrowPath.lineTo((getWidth()/4)*3, (getHeight()/2)+60); // Tip of arrow pointing down
 				arrowPath.lineTo((getWidth()/4)*3-20, (getHeight()/2)+20);
 				arrowPath.lineTo((getWidth()/4)*3-10, (getHeight()/2)+20);
