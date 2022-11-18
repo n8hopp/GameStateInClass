0555899309bb76463cc2b3eb1ebe100993463601
millerl23 <112990842+millerl23@users.noreply.github.com>
2022-11-17 16:41:16 -0800

    Got a basic test to successfully work

---
+++ app/src/test/java/com/example/gamestateinclass/uno/UnoLocalGameTest.java
@@ -0,0 +1,26 @@
+package com.example.gamestateinclass.uno;
+
+import junit.framework.TestCase;
+
+import org.junit.Test;
+
+public class UnoLocalGameTest extends TestCase {
+
+    @Test
+    public void testCheckCardValidity() {
+        UnoLocalGame testGame = new UnoLocalGame();
+        assertEquals(4,2+2);
+    }
+
+    @Test
+    public void testMakeMove() {
+    }
+
+    @Test
+    public void testDrawCard() {
+    }
+
+    @Test
+    public void testPlaceCard() {
+    }
+}
