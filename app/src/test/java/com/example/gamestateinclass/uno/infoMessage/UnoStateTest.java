e203884ae85f1e614949e8cf71d2bb174b30b56c
millerl23 <112990842+millerl23@users.noreply.github.com>
2022-11-17 17:16:58 -0800

    Update UnoStateTest.java

--- app/src/test/java/com/example/gamestateinclass/uno/infoMessage/UnoStateTest.java
+++ app/src/test/java/com/example/gamestateinclass/uno/infoMessage/UnoStateTest.java
@@ -5,6 +5,7 @@ import junit.framework.TestCase;
 public class UnoStateTest extends TestCase {
 
     public void testCheckVictory() {
+        UnoState testState = new UnoState();
     }
 
     public void testFetchCurrentPlayer() {
