05dd7e45e1513a3ab70822afcfc494b004efe33c
HenrySchiffUP <schiff26@up.edu>
2022-11-17 17:11:04 -0800

    added tests for placeCard, reverseCard and skipCard

--- app/src/test/java/com/example/gamestateinclass/uno/UnoLocalGameTest.java
+++ app/src/test/java/com/example/gamestateinclass/uno/UnoLocalGameTest.java
@@ -2,9 +2,12 @@ package com.example.gamestateinclass.uno;
 
 import com.example.gamestateinclass.uno.infoMessage.UnoState;
 import com.example.gamestateinclass.uno.objects.Card;
+import com.example.gamestateinclass.uno.objects.CardColor;
+import com.example.gamestateinclass.uno.objects.Face;
 
 import junit.framework.TestCase;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 import java.util.ArrayList;
@@ -25,6 +28,16 @@ public class UnoLocalGameTest extends TestCase {
 
     @Test
     public void testDrawCard() {
+        UnoLocalGame testGame = new UnoLocalGame();
+        UnoState testState = (UnoState) testGame.getGameState();
+
+        ArrayList<Card> hand = testState.fetchPlayerHand(0);
+
+        int handSizeBefore = hand.size();
+        testGame.drawCard(1);
+        int handSizeAfter = hand.size();
+
+        assertEquals(handSizeBefore + 1, handSizeAfter);
 
     }
 
@@ -40,5 +53,34 @@ public class UnoLocalGameTest extends TestCase {
         Card resultCard = testState.getTopCard();
         assertEquals(testCard, resultCard);
 
+    }
+
+    @Test
+    public void testReverseCard() {
+        UnoLocalGame testGame = new UnoLocalGame();
+        UnoState testState = (UnoState) testGame.getGameState();
+
+        int playDirectionBefore = testState.getDirection().value;
+
+        Card reverseCard = new Card(CardColor.BLUE, Face.REVERSE);
+        testGame.placeCard(reverseCard);
+
+        int playDirectionAfter = testState.getDirection().value;
+
+        assertEquals(playDirectionAfter, -playDirectionBefore);
+    }
+
+    public void testSkipCard() {
+        UnoLocalGame testGame = new UnoLocalGame();
+        UnoState testState = (UnoState) testGame.getGameState();
+
+        int turnBefore = testState.getTurn();
+
+        Card skipCard = new Card(CardColor.BLUE, Face.SKIP);
+        testGame.placeCard(skipCard);
+
+        int turnAfter = testState.getTurn();
+
+        assertEquals(turnBefore + 1, turnAfter);
     }
 }
