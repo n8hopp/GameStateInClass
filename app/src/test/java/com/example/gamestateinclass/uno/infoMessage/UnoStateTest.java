2a034de95aaf9712edb6d0fb38536e0926ef2ab5
millerl23 <112990842+millerl23@users.noreply.github.com>
2022-11-17 17:15:50 -0800

    Added UnoState test

---
+++ app/src/test/java/com/example/gamestateinclass/uno/infoMessage/UnoStateTest.java
@@ -0,0 +1,18 @@
+package com.example.gamestateinclass.uno.infoMessage;
+
+import junit.framework.TestCase;
+
+public class UnoStateTest extends TestCase {
+
+    public void testCheckVictory() {
+    }
+
+    public void testFetchCurrentPlayer() {
+    }
+
+    public void testGetTopCard() {
+    }
+
+    public void testGetDirection() {
+    }
+}
