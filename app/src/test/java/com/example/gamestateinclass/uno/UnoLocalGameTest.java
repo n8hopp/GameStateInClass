600d33666e6d8821cd9c5ee67e2b1930d9036711
millerl23 <112990842+millerl23@users.noreply.github.com>
2022-11-17 16:57:01 -0800

    Implemented checkCardValidity and placeCard tests

--- app/src/test/java/com/example/gamestateinclass/uno/UnoLocalGameTest.java
+++ app/src/test/java/com/example/gamestateinclass/uno/UnoLocalGameTest.java
@@ -1,10 +1,13 @@
 package com.example.gamestateinclass.uno;
 
 import com.example.gamestateinclass.uno.infoMessage.UnoState;
+import com.example.gamestateinclass.uno.objects.Card;
 
 import junit.framework.TestCase;
 
 import org.junit.Test;
+
+import java.util.ArrayList;
 
 public class UnoLocalGameTest extends TestCase {
 
@@ -12,10 +15,11 @@ public class UnoLocalGameTest extends TestCase {
     public void testCheckCardValidity() {
         UnoLocalGame testGame = new UnoLocalGame();
         UnoState testState = (UnoState) testGame.getGameState();
+        // Top card of the seeded discard deck is always red
+        ArrayList<Card> Hand = testState.fetchPlayerHand(0);
 
-
-
-        assertEquals(4, testState.fetchPlayerHand(0));
+        assertTrue(testGame.checkCardValidity(Hand.get(0)));
+        assertFalse(testGame.checkCardValidity(Hand.get(2)));
     }
 
 
@@ -26,6 +30,15 @@ public class UnoLocalGameTest extends TestCase {
 
     @Test
     public void testPlaceCard() {
+        UnoLocalGame testGame = new UnoLocalGame();
+        UnoState testState = (UnoState) testGame.getGameState();
+        // Top card of the seeded discard deck is always red
+        ArrayList<Card> Hand = testState.fetchPlayerHand(0);
+        Card testCard = Hand.get(0);
+        testGame.placeCard(Hand.get(0));
+        // Places it's red card on deck
+        Card resultCard = testState.getTopCard();
+        assertEquals(testCard, resultCard);
 
     }
 }
