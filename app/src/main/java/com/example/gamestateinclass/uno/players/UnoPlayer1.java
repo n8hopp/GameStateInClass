11aadb93d47cb3f5eb0f40b5851bcc8882ed13bd
c1ei <paguirig25@up.edu>
2022-12-01 16:03:03 -0800

    Exit/Menu Button Implementation
    Added functionality to menu button (help, new game, quit game).
    
    Menu button still needs the functionality of save game, load game, and delete game.

--- app/src/main/java/com/example/gamestateinclass/uno/players/UnoPlayer1.java
+++ app/src/main/java/com/example/gamestateinclass/uno/players/UnoPlayer1.java
@@ -1,17 +1,26 @@
 package com.example.gamestateinclass.uno.players;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.support.v4.app.INotificationSideChannel;
 import android.util.Log;
+import android.view.MenuItem;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
+import android.widget.PopupMenu;
 import android.widget.SeekBar;
 import android.widget.TextView;
 
 import com.example.gamestateinclass.R;
 import com.example.gamestateinclass.game.GameFramework.GameMainActivity;
 import com.example.gamestateinclass.game.GameFramework.actionMessage.GameAction;
+import com.example.gamestateinclass.game.GameFramework.gameConfiguration.GameConfig;
 import com.example.gamestateinclass.game.GameFramework.infoMessage.GameInfo;
+import com.example.gamestateinclass.game.GameFramework.infoMessage.GameState;
 import com.example.gamestateinclass.game.GameFramework.players.GameHumanPlayer;
+import com.example.gamestateinclass.game.GameFramework.utilities.Logger;
+import com.example.gamestateinclass.game.GameFramework.utilities.Saving;
 import com.example.gamestateinclass.uno.DrawCardAction;
 import com.example.gamestateinclass.uno.PlaceCardAction;
 import com.example.gamestateinclass.uno.infoMessage.UnoState;
@@ -24,7 +33,7 @@ import com.example.gamestateinclass.uno.views.UnoTableView;
 import java.lang.reflect.Array;
 import java.util.ArrayList;
 
-public class UnoPlayer1 extends GameHumanPlayer implements View.OnTouchListener, SeekBar.OnSeekBarChangeListener{
+public class UnoPlayer1 extends GameHumanPlayer implements View.OnTouchListener, SeekBar.OnSeekBarChangeListener, View.OnClickListener {
 	private int layoutId;
 	private UnoTableView tableView;
 	private UnoHandView handView;
@@ -33,6 +42,7 @@ public class UnoPlayer1 extends GameHumanPlayer implements View.OnTouchListener,
 
 	private int selectedIndex;
 	private SeekBar handSeekBar;
+	private Button exitButton;
 	private int startingHandCard;
 
 	boolean moveMade = false;
@@ -70,26 +80,20 @@ public class UnoPlayer1 extends GameHumanPlayer implements View.OnTouchListener,
 		tableView.setState(gameState);
 		handView.setState(gameState);
 
-		if(gameState.getTurn() == playerNum)
-		{
+		if (gameState.getTurn() == playerNum) {
 			moveMade = false;
 			currentTurn = true;
 		}
-
-
 
 		handView.setCurrentPlayerId(playerNum);
 
 		ArrayList<Integer> displayOrder = new ArrayList<>();
 		ArrayList<Integer> tempOrder = new ArrayList<>();
 		boolean playerFound = false;
-		for(int i=0; i < gameState.getHandsSize(); i++) {
-			if(i != playerNum && !playerFound)
-			{
+		for (int i = 0; i < gameState.getHandsSize(); i++) {
+			if (i != playerNum && !playerFound) {
 				tempOrder.add(i);
-			}
-			else
-			{
+			} else {
 				displayOrder.add(i);
 				playerFound = true;
 			}
@@ -130,18 +134,18 @@ public class UnoPlayer1 extends GameHumanPlayer implements View.OnTouchListener,
 
 			// if the "fake" draw card was touched, send the drawCardAction
 			if (fakeDrawCard.getRender().isClicked(motionEvent.getX(), motionEvent.getY())
-				&& !tableView.getWildCardSelection()) {
+					&& !tableView.getWildCardSelection()) {
 
 				action = new DrawCardAction(this);
 				game.sendAction(action);
 
-			// otherwise, see if topCard was touched, if so place the currently selected card in hand
+				// otherwise, see if topCard was touched, if so place the currently selected card in hand
 			} else if (topCard.getRender().isClicked(motionEvent.getX(), motionEvent.getY())) {
 
 				Card card = myHand.get(selectedIndex);
 
 				// don't place if wild: instead bring up color prompt
-				if(!moveMade) {
+				if (!moveMade) {
 					if (card.getFace() == Face.DRAWFOUR || card.getFace() == Face.WILD) {
 						tableView.setTempWildFace(card.getFace());
 						tableView.setWildCardSelection(true);
@@ -160,10 +164,10 @@ public class UnoPlayer1 extends GameHumanPlayer implements View.OnTouchListener,
 				handView.invalidate();
 
 
-			// lastly, check if color wheel is tapped (black means out of bounds)
+				// lastly, check if color wheel is tapped (black means out of bounds)
 			} else if (tableView.getTappedColor(
-				motionEvent.getX(), motionEvent.getY()) != CardColor.BLACK
-				&& tableView.getWildCardSelection()) {
+					motionEvent.getX(), motionEvent.getY()) != CardColor.BLACK
+					&& tableView.getWildCardSelection()) {
 
 				CardColor color = tableView.getTappedColor(motionEvent.getX(), motionEvent.getY());
 				Card card = myHand.get(selectedIndex);
@@ -193,11 +197,9 @@ public class UnoPlayer1 extends GameHumanPlayer implements View.OnTouchListener,
 
 			// check each card in hand to see if it was tapped.
 			// if so, set it as selected
-			for(int i=0; i < myHand.size(); i++)
-			{
+			for (int i = 0; i < myHand.size(); i++) {
 				Card c = myHand.get(i);
-				if(c.getRender().isClicked(motionEvent.getX(), motionEvent.getY()))
-				{
+				if (c.getRender().isClicked(motionEvent.getX(), motionEvent.getY())) {
 					selectedIndex = i;
 					handView.setSelectedIndex(i);
 					handView.invalidate();
@@ -224,15 +226,15 @@ public class UnoPlayer1 extends GameHumanPlayer implements View.OnTouchListener,
 
 		// actionText = activity.findViewById(R.id.lobbyInfoText);
 		handSeekBar = activity.findViewById(R.id.scrollHandSeekBar);
+		exitButton = activity.findViewById(R.id.exitButton);
 
 		// Sets a Listener for views and buttons users can touch
 		tableView.setOnTouchListener(this);
 		handView.setOnTouchListener(this);
 		handSeekBar.setOnSeekBarChangeListener(this);
+		exitButton.setOnClickListener((View.OnClickListener) this);
 
 		//tableView.setPlayerNameText(allPlayerNames[0], allPlayerNames[1], allPlayerNames[2], allPlayerNames[3]);
-
-
 
 	}
 
@@ -256,4 +258,94 @@ public class UnoPlayer1 extends GameHumanPlayer implements View.OnTouchListener,
 	public void onStopTrackingTouch(SeekBar seekBar) {
 
 	}
+
+	@Override
+	public void onClick(View v) {
+		PopupMenu popupMenu = new PopupMenu(myActivity, v);
+		popupMenu.getMenuInflater().inflate(R.menu.game_main, popupMenu.getMenu());
+		popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+			@Override
+			public boolean onMenuItemClick(MenuItem item) {
+				switch (item.getItemId()) {
+					case R.id.menu_help:
+						final AlertDialog.Builder builder = new AlertDialog.Builder(myActivity);
+						builder.setMessage
+								("How To Play Uno By Three-Oh-Uno \n\n" +
+										"Rules of the Game: Setup \n\n" +
+										"Each Player receives seven cards at random. \n" +
+										"The remaining cards are the Draw Pile, which is placed in the center face down. \n" +
+										"The top card of the Draw Pile is turned over to create the Discard Pile. \n" +
+										"A player is randomly selected to go first, and the flow of play begins clockwise. \n\n" +
+										"Rules of the Game: Core Gameplay \n\n" +
+										"The objective is to be the first person to empty your hands of cards. \n" +
+										"Each turn, the current player will attempt to discard one of their cards into the Discard Pile. \n" +
+										"Only cards whose color or number matches the face card of the Discard Pile can be placed atop it. (Exceptions occur with special cards)\n" +
+										"If a player has no viable cards to place, they must draw one card from the draw deck. \n" +
+										"Drawing or discarding ends the players turn. \n\n" +
+										"Rules of the Game: Special Cards \n\n" +
+										"Reverse: The flow of play is reversed. \n" +
+										"Skip: The next person to play has their turn skipped. \n" +
+										"Draw 2: The next player must draw two cards and forfeit their turn. \n" +
+										"Wildcard: The player who placed this card can select what color it is. \n" +
+										"Wildcard Draw 4: The next player must draw four cards and forfeit their turn, player who placed this card can select what color it is. \n\n" +
+										"Have Fun!");
+						builder.setPositiveButton("Return to game", new DialogInterface.OnClickListener() {
+							@Override
+							public void onClick(DialogInterface dialogInterface, int i) {
+								dialogInterface.cancel();
+							}
+						});
+						AlertDialog alertDialog1 = builder.create();
+						alertDialog1.show();
+						return true;
+
+					case R.id.new_game:
+						final AlertDialog.Builder builder2 = new AlertDialog.Builder(myActivity);
+						builder2.setMessage("Are you sure you want a new game?");
+						builder2.setCancelable(true);
+						builder2.setNegativeButton("No", new DialogInterface.OnClickListener() {
+							@Override
+							public void onClick(DialogInterface dialogInterface, int i) {
+								dialogInterface.cancel();
+							}
+						});
+						builder2.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+							@Override
+							public void onClick(DialogInterface dialog, int which) {
+								myActivity.restartGame();
+							}
+						});
+						AlertDialog alertDialog2 = builder2.create();
+						alertDialog2.show();
+						return true;
+
+					case R.id.quit_game:
+						final AlertDialog.Builder builder3 = new AlertDialog.Builder(myActivity);
+						builder3.setMessage("Are you sure you want to exit the game?");
+						builder3.setCancelable(true);
+						builder3.setNegativeButton("No", new DialogInterface.OnClickListener() {
+							@Override
+							public void onClick(DialogInterface dialogInterface, int i) {
+								dialogInterface.cancel();
+							}
+						});
+						builder3.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+							@Override
+							public void onClick(DialogInterface dialogInterface, int i) {
+								myActivity.finish();
+							}
+						});
+						AlertDialog alertDialog3 = builder3.create();
+						alertDialog3.show();
+						return true;
+
+					default:
+						return false;
+				}
+			}
+		});
+		popupMenu.show();
+	}
+
 }
+
